f03570ff75a7d49dabe0c49ae95294734e79e2e9
Saurav Dahal <dsaurav2020@gmail.com>
2025-02-28 03:10:19 -0500

    Fixed Typos

--- src/components/MobileMenu.jsx
+++ src/components/MobileMenu.jsx
@@ -15,7 +15,7 @@ const MobileMenu = ({ menuOpen, setMenuOpen }) => {
         &times;
       </button>
       <a
-        href="#Home"
+        href="#home"
         onClick={() => setMenuOpen(false)}
         className={`text-2xl font-semibold text-white my-4 transform transition-transform duration-300 ${
           menuOpen ? "opacity-100 transalte-y-0" : "opacity-0 translate-y-5"
